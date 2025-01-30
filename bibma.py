d056e7c99120074cc418a74e86bef1022678dc87
JoaoGabrielTN <joaogabrieltdonascimento@gmail.com>
2025-01-30 16:11:08 -0300

    Dev: refactoring

--- bibma.py
+++ bibma.py
@@ -13,23 +13,35 @@ def check_extension(path: str):
         tail=path[index]
     return extension
 
+def open_file(path):
+    ext = check_extension(path)
+    if ext == 'xlsx':
+        df = pd.read_excel(path)
+    elif ext == 'bib':
+        with open(path, 'r+') as file:
+            df = bib.load(file)
+            df = pd.DataFrame(lib.entries)
+    elif ext == 'csv':
+        df = pd.read_csv(path)
+    else:
+        raise RunTimeError
+    df['doi'] = df['doi'].apply(lambda x: x if x.startswith('https://') else 'https://' + x)
+    df['year'] = df['year'].apply(lambda x: int(x))
+    return df
+
+
 def parse_args():
     parser = argparse.ArgumentParser('Bibtex files manipulation')
-    parser.add_argument('-i', type=str, default=None, help='Path to input file')
-    parser.add_argument('-o', type=str, default=None, help='Path to output file')
-    parser.add_argument('-d', type=str, default=None, help='Column to delete')
-    parser.add_argument('-c', type=str, default=None, help='Bibtex file to compare')
+    parser.add_argument('-i', '--input', type=str, default=None, help='Path to input file')
+    parser.add_argument('-o', '--output', type=str, default=None, help='Path to output file')
+    parser.add_argument('-d', '--delete', type=str, default=None, help='Column to delete')
+    parser.add_argument('-j', '--join', type=str, default=None, help='Bibtex file to compare')
     parser.add_argument('--datelim', type=int, default=0, help='Use with -d to delete papers published before a specific year')
     return parser.parse_args()
 
-def bibToXlsx(path: str, out: str, dcol: str=None, cdel: str= None, datelim: int=None) -> None:
-    with open(path, 'r+') as file:
-        lib = bib.load(file)
-        df = pd.DataFrame(lib.entries)
-        start = len(df)
-        df['doi'] = df['doi'].apply(lambda x: x if x.startswith('https://') else 'https://' + x)
-        df['year'] = df['year'].apply(lambda x: int(x))
-
+def bibToXlsx(path: str, out: str='newfile', dcol: str=None, cdel: str= None, datelim: int=None) -> None:
+    df = open_file(path)
+    start = len(df)
     if dcol and not cdel:
         print(f'Dropping \'{dcol}\' column')
         df = df.drop(dcol,axis=1)
@@ -57,19 +69,23 @@ def bibToXlsx(path: str, out: str, dcol: str=None, cdel: str= None, datelim: int
 
 if __name__ == "__main__":
     args = parse_args()
+    convert = False
     dcol = None
     cdel = None
     datelim = None
-    if args.i:
-        path = args.i
-    if args.o:
-        out = args.o
-    if args.d:
-        dcol = args.d
-        if args.c:
-            cdel = args.c
+
+    if args.input:
+        path = args.input
+        convert = True
+    if args.output:
+        out = args.output
+    if args.delete:
+        dcol = args.delete
+        if args.join:
+            cdel = args.join
         if args.datelim:
             datelim=args.datelim
-        
-    bibToXlsx(path, out, dcol=dcol, cdel=cdel, datelim=datelim)
+
+    if convert:
+        bibToXlsx(path, out, dcol=dcol, cdel=cdel, datelim=datelim)
 
